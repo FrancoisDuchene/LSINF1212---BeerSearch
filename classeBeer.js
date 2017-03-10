64336d395a5b86f8c6c59e510c414f904be58409
julien1bastin <julienbastin96@hotmail.com>
2017-03-10 19:41:51 +0100

    dezip file

---
+++ classeBeer.js
@@ -0,0 +1,18 @@
+export class Beer
+{
+  getNom() {// renvoie le nom de la bière
+    throw new Error("not yet implemented");
+  }
+  getDegree(){//renvoie le taux d'alcohol de la bière
+    throw new Error("not yet implemented");
+  }
+  getSellPlace(){// renvoie les points de vente qui disposent de la bière
+    throw new Error("not yet implemented");
+  }
+  getType(){ // renvoie le type de la bière
+    throw new Error("not yet implemented");
+  }
+  getTaste(){// renvoie le goût de la bière
+    throw new Error("not yet implemented");
+  }
+}
